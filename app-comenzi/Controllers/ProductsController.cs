f20c970b907e09e8fb25f770864f4b3a9d1a022c
Socea Tiberiu <soceatiberiuv@gmail.com>
2019-03-13 01:32:50 +0200

    first product implementation

---
+++ app-comenzi/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using app_comenzi.Models;
+
+namespace app_comenzi.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products/Random
+        public ActionResult Random()
+        {
+            var product = new Product() { Id = 1, Name = "Cartofi copti", Description = "Faimosii cartofi copti", Price = 5.5f, Supply = 200 };
+
+            return View(product);
+        }
+    }
+}
---
+++ app-comenzi/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace app_comenzi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public int Supply { get; set; }
+    }
+}
